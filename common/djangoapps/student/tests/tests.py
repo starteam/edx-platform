f91905061efaf95b57fd0ee9c45d7c200274b888
Will Daly <will@edx.org>
2014-08-28 07:03:51 -0400

    Bugfix for ECOM-199: Visitors removed from auto-enroll pool if they need to authenticate while attempting to enroll Sets a session variable as soon as a user enters the experimental condition to ensure that they stay in that condition, even if they later access the control URL (e.g. from a redirect on the login page)

--- common/djangoapps/student/tests/tests.py
+++ common/djangoapps/student/tests/tests.py
@@ -15,6 +15,7 @@ from django.test.utils import override_settings
 from django.test.client import RequestFactory, Client
 from django.contrib.auth.models import User, AnonymousUser
 from django.core.urlresolvers import reverse, NoReverseMatch
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.http import HttpResponse
 from unittest.case import SkipTest
 
@@ -602,8 +603,17 @@ class PaidRegistrationTest(ModuleStoreTestCase):
 
     @unittest.skipUnless(settings.FEATURES.get('ENABLE_SHOPPING_CART'), "Shopping Cart not enabled in settings")
     def test_change_enrollment_add_to_cart(self):
-        request = self.req_factory.post(reverse('change_enrollment'), {'course_id': self.course.id.to_deprecated_string(),
-                                                                       'enrollment_action': 'add_to_cart'})
+        request = self.req_factory.post(
+            reverse('change_enrollment'), {
+                'course_id': self.course.id.to_deprecated_string(),
+                'enrollment_action': 'add_to_cart'
+            }
+        )
+
+        # Add a session to the request
+        SessionMiddleware().process_request(request)
+        request.session.save()
+
         request.user = self.user
         response = change_enrollment(request)
         self.assertEqual(response.status_code, 200)
